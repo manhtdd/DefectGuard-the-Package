37cf42cd6754697433295398a11362e7fb48959e
npt-1707 <npthanh132@gmail.com>
2024-03-25 03:55:12 +0700

    fix import error

--- defectguard/__init__.py
+++ defectguard/__init__.py
@@ -5,5 +5,4 @@ from .models.simcom.warper import SimCom
 from .models.lapredict.warper import LAPredict
 from .models.tlel.warper import TLEL
 from .models.jitline.warper import JITLine
-from .extractor.RepositoryExtractor import RepositoryExtractor
 # EARL
