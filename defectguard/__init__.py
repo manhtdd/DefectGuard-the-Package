225113675ab8e20a361d1eaee8a0af7a493d3003
manhtdd <manh.td120901@gmail.com>
2024-03-29 22:22:42 +0700

    Remove redunce code

--- defectguard/__init__.py
+++ defectguard/__init__.py
@@ -2,7 +2,6 @@ from .cli import main, __version__
 from .models.deepjit.warper import DeepJIT
 from .models.cc2vec.warper import CC2Vec
 from .models.simcom.warper import SimCom
-from .models.lapredict.warper import LAPredict
 from .models.tlel.warper import TLEL
 from .models.lr.warper import LogisticRegression
 from .models.lapredict.warper import LAPredict
