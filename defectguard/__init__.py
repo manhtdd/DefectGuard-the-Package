4b2ddb1628f01c6b971a34454eae368c08f7f818
manhtdd <manh.td120901@gmail.com>
2024-03-29 22:20:54 +0700

    Update __init__.py

--- defectguard/__init__.py
+++ defectguard/__init__.py
@@ -4,5 +4,6 @@ from .models.cc2vec.warper import CC2Vec
 from .models.simcom.warper import SimCom
 from .models.lapredict.warper import LAPredict
 from .models.tlel.warper import TLEL
-from .models.jitline.warper import JITLine
+from .models.lr.warper import LogisticRegression
+from .models.lapredict.warper import LAPredict
 # EARL
