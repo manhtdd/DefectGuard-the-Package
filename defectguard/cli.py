04772b4ba9ced6b999fa3262b5b729d3c8e01cc6
manhtdd <manh.td120901@gmail.com>
2024-04-06 10:37:44 +0700

    Add training

--- defectguard/cli.py
+++ defectguard/cli.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import numpy as np
 from .inferencing import inferencing
 from .mining import mining
+from .training import training
 
 __version__ = "0.1.33"
 
@@ -24,13 +25,10 @@ def main(args=None):
         args = sys.argv[1:]
 
     available_languages = ["Python", "Java", "C++", "C", "C#", "JavaScript", "TypeScript", "Ruby", "PHP", "Go", "Swift"]
-    models = ["deepjit", "cc2vec", "simcom", "lapredict", "tlel", "jitline", "la", "lr"]
-    dataset = ["gerrit", "go", "platform", "jdt", "qt", "openstack"]
+    models = ["deepjit", "simcom", "lapredict", "tlel", "la", "lr"]
     modes = ["local", "remote"]
 
     common_parser = argparse.ArgumentParser(add_help=False)
-    common_parser.add_argument("-debug", action="store_true", help="Turn on system debug print")
-    common_parser.add_argument("-log_to_file", action="store_true", help="Logging to file instead of stdout")
     common_parser.add_argument("-dg_save_folder", type=str, required=True, help="")
     common_parser.add_argument("-mode", type=str, default="local", help="Mode of extractor", choices=modes)
     common_parser.add_argument("-repo_name", type=str, required=True, help="Repo name")
@@ -45,9 +43,7 @@ def main(args=None):
     inferencing_parser = argparse.ArgumentParser(parents=[common_parser], add_help=False)
     inferencing_parser.set_defaults(func=inferencing)
     inferencing_parser.add_argument("-models", nargs="+", type=str, default=[], choices=models, help="List of deep learning models")
-    inferencing_parser.add_argument("-dataset",type=str,default="openstack",choices=dataset,help="Dataset's name",)
-    inferencing_parser.add_argument("-cross", action="store_true", help="Cross project")
-    inferencing_parser.add_argument("-uncommit", action="store_true", help="Include uncommit in list when using -top",)
+    inferencing_parser.add_argument("-uncommit", action="store_true", help="Include uncommit in list when using -top")
     inferencing_parser.add_argument("-device", type=str, default="cpu", help="Eg: cpu, cuda, cuda:1")
     inferencing_parser.add_argument("-sort", action="store_true", help="Sort output of model by predict score")
     inferencing_parser.add_argument("-vsc", action="store_true", help="Output for vsc")
@@ -56,11 +52,22 @@ def main(args=None):
     inferencing_parser.add_argument("-commit_hash", nargs="+", type=str, default=[], help="List of commit hashes")
     inferencing_parser.add_argument("-top", type=int, default=0, help="Number of top commits")
 
+    training_parser = argparse.ArgumentParser(parents=[common_parser], add_help=False)
+    training_parser.set_defaults(func=training)
+    training_parser.add_argument("-models", nargs="+", type=str, default=[], choices=models, help="List of deep learning models")
+    training_parser.add_argument("-dataset",type=str,default="", help="")
+    training_parser.add_argument("-dictionary",type=str,default="", help="")
+    training_parser.add_argument("-hyperparameters",type=str,default="", help="")
+    training_parser.add_argument("-device", type=str, default="cpu", help="Eg: cpu, cuda, cuda:1")
+
     parser = argparse.ArgumentParser(prog="DefectGuard", description="A tool for mining, training, evaluating for Just-in-Time Defect Prediction")
     parser.add_argument("-version", action="version", version="%(prog)s " + __version__)
+    parser.add_argument("-debug", action="store_true", help="Turn on system debug print")
+    parser.add_argument("-log_to_file", action="store_true", help="Logging to file instead of stdout")
     subparsers = parser.add_subparsers()
     subparsers.add_parser('mining', parents=[mining_parser], help='Mining Function')
     subparsers.add_parser('inferencing', parents=[inferencing_parser], help='Inferencing Function')
+    subparsers.add_parser('training', parents=[training_parser], help='Training Function')
 
     options = parser.parse_args(args)
 
---
+++ defectguard/training.py
@@ -0,0 +1,2 @@
+def training(params):
+    pass
