a5d809d302e3e3b95a08decc6e07121acc3e9d31
manhtdd <manh.td120901@gmail.com>
2024-04-09 23:04:06 +0700

    Rename folder

--- test_suits/docker_build_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bib/bash
-
-docker build -t defectguard:1.0 .
-# docker run -it \
-#     -v $(pwd):/app \
-#     -v $(pwd)/input:/input \
-#     --name defectguard \
-#     defectguard:1.0
--- test_suits/test_train.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-defectguard training \
-    -models deepjit \
-    -dg_save_folder . \
-    -repo_name Tic-tac-toe-Game-using-Network-Socket-APIs \
