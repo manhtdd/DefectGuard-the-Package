d128616ebaaed6626d3d2f41cda86d4150aa4bb2
manhtdd <manh.td120901@gmail.com>
2024-04-20 09:00:59 +0700

    Update script

--- scripts/test_inferencing.sh
+++ scripts/test_inferencing.sh
@@ -3,6 +3,7 @@
 defectguard -debug inferencing \
     -model deepjit \
     -dg_save_folder . \
+    -repo_path input \
     -repo_name Tic-tac-toe-Game-using-Network-Socket-APIs \
     -repo_language C \
-    -commit_hash 7953fc48f8d1c3a2f057dbefec58afcd22b6ccc8 \
+    -top 10 \
