76fc67b333666ce66dad5775577d0245e6e46bc8
manhtdd <manh.td120901@gmail.com>
2024-05-07 15:05:39 +0700

    update script

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -2,16 +2,16 @@
 
 # Array of GitHub repository URLs
 repos=(
-  # "https://github.com/iptv-org/iptv"
+  "https://github.com/iptv-org/iptv"
   "https://github.com/preactjs/preact"
-  # "https://github.com/phaserjs/phaser"
-  # "https://github.com/twbs/bootstrap"
-  # "https://github.com/moment/moment"
-  # "https://github.com/TryGhost/Ghost"
-  # "https://github.com/eslint/eslint"
-  # "https://github.com/Automattic/mongoose"
-  # "https://github.com/codemirror/codemirror5"
-  # "https://github.com/transloadit/uppy"
+  "https://github.com/phaserjs/phaser"
+  "https://github.com/twbs/bootstrap"
+  "https://github.com/moment/moment"
+  "https://github.com/TryGhost/Ghost"
+  "https://github.com/eslint/eslint"
+  "https://github.com/Automattic/mongoose"
+  "https://github.com/codemirror/codemirror5"
+  "https://github.com/transloadit/uppy"
 )
 
 # Loop through each repository URL and clone it
