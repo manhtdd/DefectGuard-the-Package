56e34444e26f567b88aa6640bd4f61357e8d2ad7
Mr-Duo <duong.nd215336@sis.hust.edu.vn>
2024-08-05 16:20:42 +0700

    add v-szz + remote crawl

---
+++ scripts/test_mining_remote.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+defectguard mining \
+    -dg_save_folder . \
+    -mode remote \
+    -repo_name Tic-tac-toe-Game-using-Network-Socket-APIs \
+    -repo_clone_url https://github.com/manhlamabc123/Tic-tac-toe-Game-using-Network-Socket-APIs \
+    -repo_language C \
+    -pyszz_path pyszz_v2
