493296793d0460303df8c63d51527ab9b20f1568
Mr-Duo <duong.nd215336@sis.hust.edu.vn>
2024-08-05 21:29:59 +0700

    update remote mine scripts

--- scripts/test_mining_remote.sh
+++ scripts/test_mining_remote.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-defectguard mining \
-    -dg_save_folder . \
-    -mode remote \
-    -repo_name Tic-tac-toe-Game-using-Network-Socket-APIs \
-    -repo_clone_url https://github.com/manhlamabc123/Tic-tac-toe-Game-using-Network-Socket-APIs \
-    -repo_language C \
-    -pyszz_path pyszz_v2
+# Define arrays for repository names and clone URLs
+repo_names=(
+    "FFmpeg" 
+    "qemu"
+)
+repo_clone_urls=(
+    "https://github.com/FFmpeg/FFmpeg" 
+    "https://github.com/qemu/qemu"
+)
+
+# Loop over the arrays
+for i in "${!repo_names[@]}"; do
+    repo_name=${repo_names[$i]}
+    repo_clone_url=${repo_clone_urls[$i]}
+    
+    # Run the defectguard mining command
+    defectguard mining \
+        -dg_save_folder . \
+        -mode remote \
+        -repo_name "$repo_name" \
+        -repo_clone_url "$repo_clone_url" \
+        -repo_language C \
+        -pyszz_path pyszz_v2
+done
