05c967f2acb87cd529818ae7d33af8179b7fc5ae
manhtdd <manh.td120901@gmail.com>
2024-08-17 00:37:24 +0700

    Change mod

--- scripts/test_mining.sh
+++ scripts/test_mining.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-defectguard -debug mining -repo_name astropy -repo_path input -repo_language Python -pyszz_path pyszz_v2
+defectguard -debug mining -repo_name astropy -repo_path input -repo_language Python -pyszz_path AI4C-SZZ
