3583afc169cf4f85cb6ca71b7af235e59b683ee8
LongK <hoanglong7802@gmail.com>
2024-08-21 22:01:08 +0700

    Localise variables in function

--- scripts/make_c_pr_dataset.sh
+++ scripts/make_c_pr_dataset.sh
@@ -22,12 +22,16 @@ token=$1
 commit_message=${2:-"Update dataset"}
 
 switch_to_PR_mode() {
-    cd pyszz_v2
+    cd pyszz_v2 || exit 1
     git checkout commit-w-file-PR-only
     cd ..
 }
 
 upload_dataset() {
+    local dataset_folder=$1
+    local repo_id=$2
+    local commit_message=$3
+    local token=$4
     if [ ! -d "$dataset_folder" ]; then
         echo "Error: Folder '$dataset_folder' does not exist."
         exit 1
@@ -39,7 +43,6 @@ upload_dataset() {
     # Check if the upload was successful
     if [ $? -eq 0 ]; then
         echo "Dataset successfully pushed to Hugging Face Hub."
-        rm -rf 
     else
         echo "Error: Failed to push dataset."
         exit 1
@@ -48,7 +51,7 @@ upload_dataset() {
 
 mine() {
     defectguard -debug -log_to_file mining \
-        -repo_name "$repo" \
+        -repo_name "$1" \
         -repo_path input/c_top10/ \
         -repo_language C \
         -pyszz_path /app/pyszz_v2 
@@ -63,6 +66,6 @@ for ((i=0; i<${#repos[@]}; i++)); do
   git clone "$repo" "$input_folder/$repo_name"
   echo "Mining $repo_name"
   mine "$repo_name"
-  upload_dataset
+  upload_dataset $dataset_folder $repo_id $commit_message $token
   rm -rf "$input_folder/$repo_name"
 done
